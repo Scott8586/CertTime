ec65d4750c19fcdf365dff913db9fef74459dfc7
Scott Presnell <srp@tworoads.net>
2019-06-02 10:35:33 -0700

    Added argument/file handling

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# CertTime
-Determine time left on a certificate
+## CertTime
+Determine time left on a certificate.  This code is a compilation of a couple of things I saw
+on the internet and stack overflow for extracting the time left on a certificate.  Here I
+added printing the serial number, and take multiple certs on the command line.
+The function
+
+```
+	ssl._ssl._test_decode_cert()
+```
+
+is undocumented and not guarenteed to exist?
--- cert_decode.py
+++ cert_decode.py
@@ -1,19 +1,43 @@
 #!/usr/bin/env python
 
+import argparse
 import sys
 import os
 import ssl
+import datetime
 from pprint import pprint as pp
 
+def ssl_expires_in(serial_number, remaining, buffer_days=14):
+    # if the cert expires in less than two weeks, we should reissue it
+    if remaining < datetime.timedelta(days=0):
+        # cert has already expired - uhoh!
+        print("Cert %s expired %s days ago!" % (serial_number, remaining.days))
+    elif remaining < datetime.timedelta(days=buffer_days):
+        # expires sooner than the buffer
+        print("Cert %s is nearly expired - %s more days" % (serial_number, remaining.days))
+    else:
+        # everything is fine
+        print("Cert %s is valid for %s more days" % (serial_number, remaining.days))
 
 def main():
-    cert_file_name = os.path.join(os.path.dirname(__file__), "testcert.pem")
-    try:
-        cert_dict = ssl._ssl._test_decode_cert(cert_file_name)
-        pp(cert_dict)
+    ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+    #cert_file_name = os.path.join(os.path.dirname(__file__), "testcert.pem")
 
-    except Exception as e:
-        print("Error decoding certificate: {:}".format(e))
+    parser = argparse.ArgumentParser(description='Parse a certificate and show days left')
+    parser.add_argument('-v', '--verbose', action='store_true', help='show full certificate')
+    parser.add_argument('cert', nargs='+', help='certifcate file(s)')
+    args = parser.parse_args()
+    for cert_file_name in args.cert:
+        try:
+            cert_dict = ssl._ssl._test_decode_cert(cert_file_name)
+	    serial = cert_dict['serialNumber']
+	    time_left = datetime.datetime.strptime(cert_dict['notAfter'], ssl_date_fmt) - datetime.datetime.utcnow()
+	    if args.verbose:
+                pp(cert_dict)
+	    ssl_expires_in(serial, time_left)
+
+        except Exception as e:
+            print("Error decoding certificate: {:}".format(e))
 
 
 if __name__ == "__main__":
